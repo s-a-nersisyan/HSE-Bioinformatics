c1361174265909a5b201ce108f87ecac0743583a
s-a-nersisyan <s.a.nersisyan@gmail.com>
2020-03-07 12:24:10 +0000

    Trimmomatic single end script

--- trimmomatic/run_trimmomatic_SE.bash
+++ trimmomatic/run_trimmomatic_SE.bash
@@ -8,14 +8,17 @@ IN_PATH=$1
 OUT_PATH=$2
 N_THREADS=$3
 
+TRIMMOMATIC_PATH="/home/steve/software/Trimmomatic-0.39/"
+
 # Create output folder if not already created
 mkdir -p ${OUT_PATH}
 
 # Iterate through all fastq files in IN_PATH and run fastqc
 for filename in ${IN_PATH}/*.fastq; do
 	sample_name=$(basename $filename)
-	echo "Started cutadapt for ${filename}"
-	cutadapt -b "AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC" \
-		 -a "A{100}" -m 50 -j ${N_THREADS} -o "${OUT_PATH}/${sample_name}" ${filename} > "${OUT_PATH}/${sample_name}.log"
-	echo "Finished cutadapt for ${filename}"
+	echo "Started Trimmomatic for ${filename}"
+	java -jar ${TRIMMOMATIC_PATH}/trimmomatic-0.39.jar SE -threads ${N_THREADS} ${filename} \
+				    "${OUT_PATH}/${sample_name}" ILLUMINACLIP:${TRIMMOMATIC_PATH}/adapters/TruSeq3-SE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36
+	
+	echo "Finished Trimmomatic for ${filename}"
 done

8e79093e12611f115089cca1d9806005a207f170
s-a-nersisyan <s.a.nersisyan@gmail.com>
2021-10-12 09:42:58 +0300

    bio imports

--- bio.py
+++ bio.py
@@ -1,5 +1,12 @@
+import sys
+import os
+import pickle
+
 import pandas as pd
 import numpy as np
 
 import matplotlib.pyplot as plt
 import seaborn as sns
+
+from scipy.stats import *
+from tqdm import tqdm

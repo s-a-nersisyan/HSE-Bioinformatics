61394421e9c392d00e1351105a2b15b4eb11f417
s-a-nersisyan <s.a.nersisyan@gmail.com>
2021-10-29 11:42:33 +0300

    Pandas read functions

--- bio.py
+++ bio.py
@@ -10,3 +10,11 @@ import seaborn as sns
 
 from scipy.stats import *
 from tqdm import tqdm
+
+
+def rc(path, i=0):
+    return pd.read_csv(path, index_col=i)
+
+
+def rt(path, i=0):
+    return pd.read_csv(path, sep="\t", index_col=i)

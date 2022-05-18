0fc0aa4eb090755148c71022017d6f1042d7e621
Victor Novosad <your_email@example.com>
2022-05-18 13:10:44 +0300

    added col method to bio

--- bio.py
+++ bio.py
@@ -20,3 +20,21 @@ def rc(path, i=0, h="infer"):
 
 def rt(path, i=0, h="infer"):
     return pd.read_csv(path, sep="\t", index_col=i, header=h)
+
+
+def fdr(p_values):
+    return p_values * len(p_values) / rankdata(p_values)
+
+
+def loc(df, index, return_in=False):
+    if return_in:
+        return df.loc[set(df.index)&set(index)], set(df.index)&set(index)
+
+    return df.loc[set(df.index)&set(index)]
+
+
+def col(df, cols, return_in=False):
+    if return_in:
+        return df[set(df.columns)&set(cols)], set(df.columns)&set(cols)
+
+    return df[set(df.columns)&set(cols)]

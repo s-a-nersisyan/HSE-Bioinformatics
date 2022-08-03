fa692373bcf285495ce9bcb7d3c17e3a132b792d
Victor Novosad <your_email@example.com>
2022-08-03 21:24:17 +0300

    remove ax

--- plots/clustermap.py
+++ plots/clustermap.py
@@ -1,14 +1,13 @@
 from bio import *
 
 
-def clustermap(df, show=False, save_path=None, ax=None, **kwargs):
+def clustermap(df, show=False, save_path=None, **kwargs):
     cg = sns.clustermap(
         df,
         **{
             'cmap': sns.color_palette("bwr", as_cmap=True),
             **kwargs,
         },
-        ax=ax,
     )
 
     cg.ax_heatmap.yaxis.tick_left()
